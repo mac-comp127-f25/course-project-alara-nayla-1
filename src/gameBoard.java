7dc646a33bbff885d43b2de736d62f72065e30a9
natrigueros <nayla22to@gmail.com>
2025-11-24 11:39:35 -0600

    got borders and fill and size

--- src/gameBoard.java
+++ src/gameBoard.java
@@ -1,4 +1,6 @@
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Rectangle;
+
 import java.awt.Color;
 
 public class gameBoard {
@@ -6,6 +8,12 @@ public class gameBoard {
 
     public gameBoard() {
         canvas = new CanvasWindow("PacMan but better", 900, 900);
+
+        Rectangle border = new Rectangle(20, 10, 860, 830);
+        border.setStrokeWidth(10);
+        border.setFillColor(Color.BLACK);
+        border.setStrokeColor(Color.BLUE);
+        canvas.add(border);
     }
 
     public static void main(String[] args){
