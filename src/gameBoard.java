bc50c9052d8c615dc1de4a9c61d12e9c59fb7459
natrigueros <nayla22to@gmail.com>
2025-11-24 11:14:13 -0600

    canvas

--- src/gameBoard.java
+++ src/gameBoard.java
@@ -1,12 +1,15 @@
 import edu.macalester.graphics.CanvasWindow;
+import java.awt.Color;
 
 public class gameBoard {
     private CanvasWindow canvas;
 
+    public gameBoard() {
+        canvas = new CanvasWindow("PacMan but better", 900, 900);
+    }
 
-    // public gameBoard() {
-    //     canvas = new CanvasWindow("PacMan but better", 900, 900);
-    // }
-
+    public static void main(String[] args){
+        new gameBoard();
+    }
 }
 
