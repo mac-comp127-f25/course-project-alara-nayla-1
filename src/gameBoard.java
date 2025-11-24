f4683e95b6e4df714b8bfc3f8153f6fea37e5652
ayener12 <ayener@macalester.edu>
2025-11-24 11:06:38 -0600

    yo

---
+++ src/gameBoard.java
@@ -0,0 +1,12 @@
+import edu.macalester.graphics.CanvasWindow;
+
+public class gameBoard {
+    private CanvasWindow canvas;
+
+
+    // public gameBoard() {
+    //     canvas = new CanvasWindow("PacMan but better", 900, 900);
+    // }
+
+}
+
