51751acc52267b2a642d4dbf44ddce389c888de5
ayener12 <ayener@macalester.edu>
2025-12-01 11:09:22 -0600

    Trying to make Cookies (and failing)

--- src/cookies.java
+++ src/cookies.java
@@ -1,3 +1,21 @@
-public class cookies {
-    
+import edu.macalester.graphics.Ellipse;
+
+import java.awt.Color;
+import java.awt.Shape;
+
+public class Cookies {
+    private Ellipse shape;
+    private double radius = 7;
+
+
+    public Cookies() {
+        shape = new Ellipse(x, y, radius * 2, radius * 2);
+        shape.setFillColor(color);
+        this.radius = radius;
+    }
+
+    public Ellipse getShape() {
+        return shape;
+    }
+
 }
--- src/gameBoard.java
+++ src/gameBoard.java
@@ -1,4 +1,5 @@
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsObject;
 import edu.macalester.graphics.Line;
 import edu.macalester.graphics.Rectangle;
 
@@ -7,16 +8,18 @@ import java.awt.Color;
 public class gameBoard {
     private CanvasWindow canvas;
     public Line walls;
+    public GraphicsObject cookies;
 
     public gameBoard() {
         canvas = new CanvasWindow("PacMan but better", 900, 900);
 
         Rectangle border = new Rectangle(20, 10, 860, 830);
         border.setStrokeWidth(10);
-        border.setFillColor(Color.BLACK);
+        border.setFillColor(Color.WHITE);
         border.setStrokeColor(Color.BLUE);
         canvas.add(border);
         canvas.add(walls);
+        canvas.add(cookies);
     }
 
     public static void main(String[] args){
