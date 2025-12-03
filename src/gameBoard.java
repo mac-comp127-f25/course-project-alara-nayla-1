12ed6520ece2cd8474444533732cc13414f9203a
natrigueros <nayla22to@gmail.com>
2025-12-03 11:41:00 -0600

    added walls/maze

--- src/gameBoard.java
+++ src/gameBoard.java
@@ -1,28 +1,46 @@
 import edu.macalester.graphics.CanvasWindow;
-import edu.macalester.graphics.GraphicsObject;
-import edu.macalester.graphics.Line;
 import edu.macalester.graphics.Rectangle;
 
 import java.awt.Color;
 
-public class GameBoard {
-    private CanvasWindow canvas;
-    public Line walls;
+// public class GameBoard {
+//     private CanvasWindow canvas;
+//     public Line walls;
     
 
-    public GameBoard() {
-        canvas = new CanvasWindow("PacMan but better", 900, 900);
+//     public GameBoard() {
+//         canvas = new CanvasWindow("PacMan but better", 900, 900);
 
+//         Rectangle border = new Rectangle(20, 10, 860, 830);
+//         border.setStrokeWidth(10);
+//         border.setFillColor(Color.BLACK);
+//         border.setStrokeColor(Color.BLUE);
+//         canvas.add(border);
+//         canvas.add(walls);
+//     }
+
+//     public static void main(String[] args){
+//         new GameBoard();
+//     }
+// }
+
+public class GameBoard {
+    private CanvasWindow canvas;
+    private Walls walls;
+
+    public GameBoard() {
+        canvas = new CanvasWindow("PacMan But Better", 900, 900);
+        
         Rectangle border = new Rectangle(20, 10, 860, 830);
         border.setStrokeWidth(10);
         border.setFillColor(Color.BLACK);
         border.setStrokeColor(Color.BLUE);
         canvas.add(border);
-        canvas.add(walls);
+        
+        walls = new Walls(canvas);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new GameBoard();
     }
 }
-
--- src/walls.java
+++ src/walls.java
@@ -1,12 +1,75 @@
 import java.awt.Color;
 
-import edu.macalester.graphics.Line;
 
+// public class Walls {
+
+// public Walls() {
+//     Line wall = new Line(60, 100, 30, 100);
+//     wall.setStrokeColor(Color.BLUE);
+// }
+// }
+
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Rectangle;
 
 public class Walls {
-
-public Walls() {
-    Line wall = new Line(60, 100, 30, 100);
-    wall.setStrokeColor(Color.BLUE);
-}
+    
+    public Walls(CanvasWindow canvas) {
+        createWalls(canvas);
+    }
+    
+    private void createWalls(CanvasWindow canvas) {
+        // Top left section
+        addWall(canvas, 70, 60, 150, 40);
+        addWall(canvas, 250, 60, 100, 40);
+        addWall(canvas, 70, 170, 80, 50);
+        addWall(canvas, 180, 170, 170, 50);
+        
+        // Top center section
+        addWall(canvas, 390, 60, 120, 200);
+        
+        // Top right section
+        addWall(canvas, 550, 60, 100, 40);
+        addWall(canvas, 680, 60, 150, 40);
+        addWall(canvas, 680, 170, 150, 40);
+        addWall(canvas, 550, 170, 100, 40);
+        
+        // Left side mid section
+        addWall(canvas, 70, 280, 80, 40);
+        addWall(canvas, 180, 280, 80, 130);
+        
+        // Center ghost house
+        addWall(canvas, 290, 280, 80, 40);
+        addWall(canvas, 290, 390, 80, 45);
+        addWall(canvas, 530, 280, 80, 40);
+        addWall(canvas, 530, 390, 80, 45);
+        addWall(canvas, 380, 510, 140, 40);
+        
+        // Right side mid section
+        addWall(canvas, 640, 280, 80, 130);
+        addWall(canvas, 750, 280, 80, 40);
+        
+        // Bottom left section
+        addWall(canvas, 70, 510, 80, 40);
+        addWall(canvas, 70, 620, 150, 40);
+        addWall(canvas, 250, 620, 100, 40);
+        addWall(canvas, 180, 730, 170, 40);
+        
+        // Bottom center
+        addWall(canvas, 380, 620, 140, 80);
+        
+        // Bottom right section
+        addWall(canvas, 750, 510, 80, 40);
+        addWall(canvas, 680, 620, 150, 40);
+        addWall(canvas, 550, 620, 100, 40);
+        addWall(canvas, 550, 730, 170, 40);
+    }
+    
+    private void addWall(CanvasWindow canvas, double x, double y, double width, double height) {
+        Rectangle wall = new Rectangle(x, y, width, height);
+        wall.setFillColor(Color.BLUE);
+        wall.setStrokeColor(Color.BLUE);
+        wall.setStrokeWidth(1);
+        canvas.add(wall);
+    }
 }
