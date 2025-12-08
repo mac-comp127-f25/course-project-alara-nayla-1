1f0cf17dde701469318373213b7e75ca075dcf05
natrigueros <nayla22to@gmail.com>
2025-12-08 11:11:15 -0600

    Update gameBoard.java

--- src/gameBoard.java
+++ src/gameBoard.java
@@ -3,27 +3,6 @@ import edu.macalester.graphics.Rectangle;
 
 import java.awt.Color;
 
-// public class GameBoard {
-//     private CanvasWindow canvas;
-//     public Line walls;
-    
-
-//     public GameBoard() {
-//         canvas = new CanvasWindow("PacMan but better", 900, 900);
-
-//         Rectangle border = new Rectangle(20, 10, 860, 830);
-//         border.setStrokeWidth(10);
-//         border.setFillColor(Color.BLACK);
-//         border.setStrokeColor(Color.BLUE);
-//         canvas.add(border);
-//         canvas.add(walls);
-//     }
-
-//     public static void main(String[] args){
-//         new GameBoard();
-//     }
-// }
-
 public class GameBoard {
     private CanvasWindow canvas;
     private Walls walls;
